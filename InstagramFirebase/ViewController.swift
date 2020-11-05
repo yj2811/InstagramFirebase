16a233462640ddcd06c6623abdb8fe567ca55e1c
yj2811 <yogakshijaiman@gmail.com>
2020-11-06 00:37:37 +0530

    Initial Commit

---
+++ InstagramFirebase/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  InstagramFirebase
+//
+//  Created by Yogakshi Jaiman on 06/11/20.
+//  Copyright © 2020 Yogakshi Jaiman. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
